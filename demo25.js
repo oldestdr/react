5bf2e8690fa1f371c9b4f16e72dddbb525717d0e
oldestdr <paymkaon@gmail.com>
2025-05-26 07:07:33 +0545

    javascript

--- demo25.js
+++ demo25.js
@@ -1,2 +1,25 @@
-console.log('hellogit ')
-console.log("lets learn js")
+const UserData=[{
+    Id:1, name:"abdul", gender:"male", age:20, occupation:undefined},
+    {Id:2, name:"rabina", gender:"female", age:21, occupation:intern},
+    {Id:3, name:"sabina", gender:"female", age:20, occupation:SWE},
+    {Id:3, name:"rabin", gender:"male", age:19, occupation:intern},
+    {Id:4, name:"nabin", gender:"male", age:18, occupation:undefined}
+
+]
+
+const response={
+    status:200,
+    message:"User data fetched succuessfully",
+    data:UserData
+}
+
+const fetchUserNames=()=>{
+    if(response.status===200)
+        {response.data.map()
+
+    }else{
+        const message="Api fetch failed"
+        return message
+    }
+}
+console.log(fetchUserNames())
