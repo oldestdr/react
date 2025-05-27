7857253e7d25c805b1cb66a1fdf423959aaf4b44
oldestdr <paymkaon@gmail.com>
2025-05-27 08:41:54 +0545

    javascript

--- demo25.js
+++ demo25.js
@@ -1,25 +1,21 @@
-const UserData=[{
-    Id:1, name:"abdul", gender:"male", age:20, occupation:undefined},
-    {Id:2, name:"rabina", gender:"female", age:21, occupation:intern},
-    {Id:3, name:"sabina", gender:"female", age:20, occupation:SWE},
-    {Id:3, name:"rabin", gender:"male", age:19, occupation:intern},
-    {Id:4, name:"nabin", gender:"male", age:18, occupation:undefined}
-
-]
-
-const response={
-    status:200,
-    message:"User data fetched succuessfully",
-    data:UserData
+const userData={
+    name:"tripal",
+    email:"tri@gmail.com",
+    phone:9876543224,
+    hasPassedOut:false,
+    occupation:undefined
+}
+const apiResponse={
+    status:404,
+    message:"not found",
+    data:{}
 }
 
-const fetchUserNames=()=>{
-    if(response.status===200)
-        {response.data.map()
-
-    }else{
-        const message="Api fetch failed"
-        return message
-    }
+const thisIsFinalUSerData={}
+const populateData=()=>{
+    if{apiResponse.status ===200 && apiResponse.data}{
+        thisIsFinalUSerData=apiResponse.data
+    }else[
+        thisIsFinalUSerData=userData
+    ]
 }
-console.log(fetchUserNames())
